e9df700cec545a05044abf2dc7fdcd16b61dfed9
惊蛰 <100140586+o-i-i-o@users.noreply.github.com>
2025-06-20 17:06:27 +0800

    Update prepare_assets.sh
    1.修复yarn异常 2.修复admin-lte路径异常

--- prepare_assets.sh
+++ prepare_assets.sh
@@ -1,28 +1,50 @@
 #!/usr/bin/env bash
 set -e
 
-RDIR=$(dirname "$0")
-DIR="${RDIR}/.yarn/*/*"
+DIR=$(dirname "$0")
+
 # install node modules
 YARN=yarn
 [ -x /usr/bin/lsb_release ] && [ -n "`lsb_release -i | grep Debian`" ] && YARN=yarnpkg
+
+# 使用 workspaces focus 替代 install --production
 $YARN workspaces focus --production
-# Copy admin-lte dist
-mkdir -p "${DIR}/assets/dist/js" "${DIR}/assets/dist/css" && \
-  cp -r "${DIR}/node_modules/admin-lte/dist/js/adminlte.min.js" "${DIR}/assets/dist/js/adminlte.min.js" && \
-  cp -r "${DIR}/node_modules/admin-lte/dist/css/adminlte.min.css" "${DIR}/assets/dist/css/adminlte.min.css"
+
+# 动态查找admin-lte目录
+ADMIN_LTE_UNPLUGGED_PATH=$(find "${DIR}/.yarn/unplugged" -type d -name "admin-lte-npm-*" -print -quit)
+
+if [ -z "$ADMIN_LTE_UNPLUGGED_PATH" ]; then
+  echo "Error: admin-lte package not found in .yarn/unplugged directory!"
+  exit 1
+fi
+
+ADMIN_LTE_PATH="${ADMIN_LTE_UNPLUGGED_PATH}/node_modules/admin-lte"
+
+# 检查 dist 目录是否存在
+if [ -d "${ADMIN_LTE_PATH}/dist" ]; then
+  mkdir -p "${DIR}/assets/dist/js" "${DIR}/assets/dist/css" && \
+  cp -r "${ADMIN_LTE_PATH}/dist/js/adminlte.min.js" "${DIR}/assets/dist/js/" && \
+  cp -r "${ADMIN_LTE_PATH}/dist/css/adminlte.min.css" "${DIR}/assets/dist/css/"
+else
+  echo "Error: admin-lte/dist directory not found!"
+  exit 1
+fi
+
+# 检查 plugins 目录是否存在
+if [ -d "${ADMIN_LTE_PATH}/plugins" ]; then
+  mkdir -p "${DIR}/assets/plugins" && \
+  cp -r "${ADMIN_LTE_PATH}/plugins/jquery" \
+        "${ADMIN_LTE_PATH}/plugins/fontawesome-free" \
+        "${ADMIN_LTE_PATH}/plugins/bootstrap" \
+        "${ADMIN_LTE_PATH}/plugins/icheck-bootstrap" \
+        "${ADMIN_LTE_PATH}/plugins/toastr" \
+        "${ADMIN_LTE_PATH}/plugins/jquery-validation" \
+        "${ADMIN_LTE_PATH}/plugins/select2" \
+        "${DIR}/assets/plugins/"
+else
+  echo "Error: admin-lte/plugins directory not found!"
+  exit 1
+fi
 
 # Copy helper js
 cp -r "${DIR}/custom" "${DIR}/assets"
-
-# Copy plugins
-mkdir -p "${DIR}/assets/plugins" && \
-  cp -r "${DIR}/node_modules/admin-lte/plugins/jquery" \
-  "${DIR}/node_modules/admin-lte/plugins/fontawesome-free" \
-  "${DIR}/node_modules/admin-lte/plugins/bootstrap" \
-  "${DIR}/node_modules/admin-lte/plugins/icheck-bootstrap" \
-  "${DIR}/node_modules/admin-lte/plugins/toastr" \
-  "${DIR}/node_modules/admin-lte/plugins/jquery-validation" \
-  "${DIR}/node_modules/admin-lte/plugins/select2" \
-  "${DIR}/node_modules/jquery-tags-input" \
-  "${DIR}/assets/plugins/"
